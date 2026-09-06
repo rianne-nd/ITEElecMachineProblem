3e816ef6cb57b8177c0ace00dedfb3996a477999
Your GitHub Name <edriannejohn.puno.cics@ust.edu.ph>
2026-09-06 17:37:26 +0800

    Set MainController.Login as default route, add new layout
    - Changed default route to MainController's Login action
    - Added MainController (Index, Login, etc.)
    - Updated _MainLayout.cshtml with Materialize, icons, SweetAlert2, and nav bar
    - Added Login.cshtml view using the new layout

---
+++ ITEElecMachineProblem/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITEElecMachineProblem.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact() {
+            return View();
+        }
+
+    }
+}
